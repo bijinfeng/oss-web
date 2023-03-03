e2e96015eeaa0371b204d3dd4b07e71e2cc7996b
bijinfeng <bijinfeng@bytedance.com>
2023-03-04 02:21:16 +0800

    fix: fix base url error

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default ({ mode }: { mode: string }) => {
   const isDev = ENV.NODE_ENV === "development";
 
   return defineConfig({
-    base: isDev ? "/" : "/music-motion-x/",
+    base: isDev ? "/" : "/oss-web/",
     plugins: [react()],
     build: {
       target: "esnext",

6121dbc336f097fe032ad56fbc18db06393c216c
bijinfeng <bijinfeng@users.noreply.github.com>
2023-03-11 09:28:29 +0000

    deploy: a245171599c38da851b95c33efe1d41953fc2f66

--- assets/index-5bc79f77.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-e8aa6659.js";const e=()=>r("div",{children:"confirm page"});export{e as default};
---
+++ assets/index-842a07dd.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b60d8d4e.js";const e=()=>r("div",{children:"confirm page"});export{e as default};
